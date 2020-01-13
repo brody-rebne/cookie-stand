b885a72fc2540dd05683f901858be960fa65a067
brody-rebne <bsrebne@gmail.com>
2020-01-13 14:17:13 -0800

    added functionality to return hourly customers as an array

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function getRandom(max, min) {
+  return (Math.random() * (max - min)) + min;
+}
+
+var seattle = {
+  name: 'Seattle',
+  avgSale: 6.3,
+  maxCust: 65,
+  minCust: 23,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  hourlyCust: function(hourIndex){
+    var hourlyCust = [];
+    for (var i=0; i<this.hours.length; i++) {
+      var hourCust = getRandom(this.maxCust, this.minCust);
+      hourlyCust.push(hourCust);
+    }
+    return hourlyCust[hourIndex];
+  }
+};
+
+console.log(seattle.hourlyCust(2));
