f2fe1c2ffaee935584bd16701b2ee17e4fc09b71
brody-rebne <bsrebne@gmail.com>
2020-01-14 14:31:23 -0800

    refactored js to use constructors and prototypes

--- app.js
+++ app.js
@@ -1,68 +1,67 @@
 'use strict';
 
+// rng function for generating random sales data
 function getRandom(max, min) {
   return (Math.random() * (max - min) + min);
 }
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-var seattle = {
-  name: 'Seattle',
-  avgSale: 6.3,
-  maxCust: 65,
-  minCust: 23,
-  getHourSales: function(){
-    var hourCust = getRandom(this.maxCust, this.minCust);
-    var hourSales = (hourCust * this.avgSale);
-    return Math.floor(hourSales);
-  }
-};
-
-var tokyo = {
-  name: 'Tokyo',
-  avgSale: 1.2,
-  maxCust: 24,
-  minCust: 3,
-  getHourlySales: function(hourIndex){
-    var hourlyCust = [];
-    var hourlySales = [];
-    for (var i=0; i<hours.length; i++) {
-      var hourCust = getRandom(this.maxCust, this.minCust);
-      hourlyCust.push(hourCust);
-    }
-    for (var j=0; j<hours.length; j++) {
-      var hourSales = (hourlyCust[j] * this.avgSale);
-      hourlySales.push(hourSales);
-    }
-    return Math.floor(hourlySales[hourIndex]);
-  }
-};
-
-// var locations = [seattle, tokyo];
-
-// function renderSales(locationName) {
-//   var locationID = document.getElementById(locationName.toString());
+// function quickAppend(parent, child, type, content) {
+//   child = document.createElement(type);
+//   child.textcontent = content;
+//   parent.appendChild(child);
 // }
 
-var seattleHeader = document.getElementById('seattleHeader');
-seattleHeader.textContent = `${seattle.name} Sales:`;
-
-var seattleUL = document.getElementById('seattleUL');
-var seattleHourSales = 0;
-var seattleHourlySales = [];
-var seattleTotalSales = 0;
-for (var i = 0; i<hours.length; i++) {
-  var seattleLI = document.createElement('li');
-  seattleHourSales = seattle.getHourSales();
-  seattleHourlySales.push(seattleHourSales);
-  seattleTotalSales += seattleHourSales;
-  seattleLI.textContent = `${hours[i]} sales: ${seattle.getHourSales()} cookies`;
-  seattleUL.appendChild(seattleLI);
+// constructor function for creating stores
+function Store(name, avgSale, maxCust, minCust) {
+  this.name = name;
+  this.avgSale = avgSale;
+  this.maxCust = maxCust;
+  this.minCust = minCust;
+  this.dailySales = 0;
+  this.hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+  this.hourlySales = [];
 }
 
-var seattleTotalLI = document.createElement('li');
-seattleTotalLI.textContent = `Total daily sales: ${seattleTotalSales}`;
-seattleUL.appendChild(seattleTotalLI);
+// generates array of random sales numbers
+Store.prototype.getRandomSales = function() {
+  for (var i=0; i<this.hours.length; i++) {
+    this.hourlySales.push(Math.floor(getRandom(this.maxCust, this.minCust) * this.avgSale));
+    this.dailySales += Math.floor(this.hourlySales[i]);
+  }
+};
+
+// renders the random sales numbers in the ui
+Store.prototype.renderSales = function() {
+  this.getRandomSales();
+  var storesArticle = document.getElementById('stores');
+  var cityHeader = document.createElement('h1');
+  var cityUL = document.createElement('ul');
+  cityHeader.textContent = this.name;
+  storesArticle.appendChild(cityHeader);
+  storesArticle.appendChild(cityUL);
+  for (var i=0; i<this.hours.length; i++) {
+    var hourLI = document.createElement('li');
+    hourLI.textContent = `${this.hours[i]}: ${this.hourlySales[i]} cookies`;
+    cityUL.appendChild(hourLI);
+    console.log('i looped!');
+  }
+  var totalLI = document.createElement('li');
+  totalLI.textContent = `Total: ${this.dailySales} cookies`;
+  cityUL.appendChild(totalLI);
+};
+
+// instantiating store objects
+var seattle = new Store('Seattle', 6.3, 65, 23);
+var tokyo = new Store('Tokyo', 1.2, 24, 3);
+var dubai = new Store('Dubai', 3.7, 38, 11);
+var paris = new Store('Paris', 2.3, 38, 20);
+var lima = new Store('Lima', 4.6, 16, 3);
+
+seattle.renderSales();
+tokyo.renderSales();
+dubai.renderSales();
+paris.renderSales();
+lima.renderSales();
 
 // ## QUESTIONS ##
 // make getHourSales() global
