48db160d025766a1a3a1fda12d92968881b2ac5c
brody-rebne <bsrebne@gmail.com>
2020-01-13 16:05:52 -0800

    added functionality to return sales/hour for every location

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 function getRandom(max, min) {
   return (Math.random() * (max - min)) + min;
 }
@@ -10,14 +11,127 @@ var seattle = {
   maxCust: 65,
   minCust: 23,
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-  hourlyCust: function(hourIndex){
+  getHourlySales: function(hourIndex){
     var hourlyCust = [];
+    var hourlySales = [];
     for (var i=0; i<this.hours.length; i++) {
       var hourCust = getRandom(this.maxCust, this.minCust);
       hourlyCust.push(hourCust);
     }
-    return hourlyCust[hourIndex];
+    for (var j=0; j<this.hours.length; j++) {
+      var hourSales = (hourlyCust[j] * this.avgSale);
+      hourlySales.push(hourSales);
+    }
+    console.log(`I am working! The hourlySales index is  ${hourlySales[hourIndex]}`);
+    return hourlySales[hourIndex];
   }
 };
 
-console.log(seattle.hourlyCust(2));
+var tokyo = {
+  name: 'Tokyo',
+  avgSale: 1.2,
+  maxCust: 24,
+  minCust: 3,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  getHourlySales: function(hourIndex){
+    var hourlyCust = [];
+    var hourlySales = [];
+    for (var i=0; i<this.hours.length; i++) {
+      var hourCust = getRandom(this.maxCust, this.minCust);
+      hourlyCust.push(hourCust);
+    }
+    for (var j=0; j<this.hours.length; j++) {
+      var hourSales = (hourlyCust[j] * this.avgSale);
+      hourlySales.push(hourSales);
+    }
+    console.log(`I am working! The hourlySales index is  ${hourlySales[hourIndex]}`);
+    return hourlySales[hourIndex];
+  }
+};
+
+var dubai = {
+  name: 'Dubai',
+  avgSale: 3.7,
+  maxCust: 38,
+  minCust: 11,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  getHourlySales: function(hourIndex){
+    var hourlyCust = [];
+    var hourlySales = [];
+    for (var i=0; i<this.hours.length; i++) {
+      var hourCust = getRandom(this.maxCust, this.minCust);
+      hourlyCust.push(hourCust);
+    }
+    for (var j=0; j<this.hours.length; j++) {
+      var hourSales = (hourlyCust[j] * this.avgSale);
+      hourlySales.push(hourSales);
+    }
+    console.log(`I am working! The hourlySales index is  ${hourlySales[hourIndex]}`);
+    return hourlySales[hourIndex];
+  }
+};
+
+var paris = {
+  name: 'Paris',
+  avgSale: 2.3,
+  maxCust: 38,
+  minCust: 20,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  getHourlySales: function(hourIndex){
+    var hourlyCust = [];
+    var hourlySales = [];
+    for (var i=0; i<this.hours.length; i++) {
+      var hourCust = getRandom(this.maxCust, this.minCust);
+      hourlyCust.push(hourCust);
+    }
+    for (var j=0; j<this.hours.length; j++) {
+      var hourSales = (hourlyCust[j] * this.avgSale);
+      hourlySales.push(hourSales);
+    }
+    console.log(`I am working! The hourlySales index is  ${hourlySales[hourIndex]}`);
+    return hourlySales[hourIndex];
+  }
+};
+
+var lima = {
+  name: 'Lima',
+  avgSale: 4.6,
+  maxCust: 16,
+  minCust: 2,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  getHourlySales: function(hourIndex){
+    var hourlyCust = [];
+    var hourlySales = [];
+    for (var i=0; i<this.hours.length; i++) {
+      var hourCust = getRandom(this.maxCust, this.minCust);
+      hourlyCust.push(hourCust);
+    }
+    for (var j=0; j<this.hours.length; j++) {
+      var hourSales = (hourlyCust[j] * this.avgSale);
+      hourlySales.push(hourSales);
+    }
+    console.log(`I am working! The hourlySales index is  ${hourlySales[hourIndex]}`);
+    return hourlySales[hourIndex];
+  }
+};
+
+function getLocationHourlySales(objectName) {
+  var locationHourlySales = [];
+  for (var i=0; i<seattle.hours.length; i++) {
+    var locationHourSales = objectName.getHourlySales(i);
+    locationHourlySales.push(locationHourSales);
+    console.log(locationHourlySales[i]);
+  }
+  console.log(locationHourlySales);
+  return locationHourlySales;
+}
+
+var locations = [seattle, tokyo, dubai, paris, lima];
+
+function getAllLocationHourlySales() {
+  for (var i=0; i<locations.length; i++) {
+    getLocationHourlySales(locations[i]);
+  }
+}
+
+getAllLocationHourlySales();
