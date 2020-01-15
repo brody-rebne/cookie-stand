e9fa84ca410271b9f50309031725f31005e7eb98
brody-rebne <bsrebne@gmail.com>
2020-01-14 18:42:09 -0800

    refactored objects into constructors, added functionality to render all sales and totals into an html table

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ function getRandom(max, min) {
 //   parent.appendChild(child);
 // }
 
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+var locations = [];
+var grandTotalSales = 0;
+
+var salesTable = document.getElementById('salesTable');
+
 // constructor function for creating stores
 function Store(name, avgSale, maxCust, minCust) {
   this.name = name;
@@ -18,13 +25,13 @@ function Store(name, avgSale, maxCust, minCust) {
   this.maxCust = maxCust;
   this.minCust = minCust;
   this.dailySales = 0;
-  this.hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
   this.hourlySales = [];
+  locations.push(this);
 }
 
 // generates array of random sales numbers
 Store.prototype.getRandomSales = function() {
-  for (var i=0; i<this.hours.length; i++) {
+  for (var i=0; i<hours.length; i++) {
     this.hourlySales.push(Math.floor(getRandom(this.maxCust, this.minCust) * this.avgSale));
     this.dailySales += Math.floor(this.hourlySales[i]);
   }
@@ -33,21 +40,19 @@ Store.prototype.getRandomSales = function() {
 // renders the random sales numbers in the ui
 Store.prototype.renderSales = function() {
   this.getRandomSales();
-  var storesArticle = document.getElementById('stores');
-  var cityHeader = document.createElement('h1');
-  var cityUL = document.createElement('ul');
-  cityHeader.textContent = this.name;
-  storesArticle.appendChild(cityHeader);
-  storesArticle.appendChild(cityUL);
-  for (var i=0; i<this.hours.length; i++) {
-    var hourLI = document.createElement('li');
-    hourLI.textContent = `${this.hours[i]}: ${this.hourlySales[i]} cookies`;
-    cityUL.appendChild(hourLI);
-    console.log('i looped!');
+  var row = document.createElement('tr');
+  salesTable.appendChild(row);
+  var storeCell = document.createElement('th');
+  storeCell.textContent = this.name;
+  row.appendChild(storeCell);
+  for(var i=0; i<this.hourlySales.length; i++) {
+    var hourlySalesCell = document.createElement('td');
+    hourlySalesCell.textContent = this.hourlySales[i];
+    row.appendChild(hourlySalesCell);
   }
-  var totalLI = document.createElement('li');
-  totalLI.textContent = `Total: ${this.dailySales} cookies`;
-  cityUL.appendChild(totalLI);
+  var storeTotalCell = document.createElement('th');
+  storeTotalCell.textContent = (this.dailySales);
+  row.appendChild(storeTotalCell);
 };
 
 // instantiating store objects
@@ -57,11 +62,56 @@ var dubai = new Store('Dubai', 3.7, 38, 11);
 var paris = new Store('Paris', 2.3, 38, 20);
 var lima = new Store('Lima', 4.6, 16, 3);
 
-seattle.renderSales();
-tokyo.renderSales();
-dubai.renderSales();
-paris.renderSales();
-lima.renderSales();
+
+function renderHeader() {
+  var headerRow = document.createElement('tr');
+  salesTable.appendChild(headerRow);
+  var emptyCell = document.createElement('th');
+  headerRow.appendChild(emptyCell);
+  for(var i=0; i<hours.length; i++) {
+    var timeCell = document.createElement('th');
+    timeCell.textContent = hours[i];
+    headerRow.appendChild(timeCell);
+  }
+  var storeTotalHeaderCell = document.createElement('th');
+  storeTotalHeaderCell.textContent = ('TOTAL');
+  headerRow.appendChild(storeTotalHeaderCell);
+}
+
+function renderBody() {
+  for(var i=0; i<locations.length; i++) {
+    locations[i].renderSales();
+  }
+}
+
+function renderFooter() {
+  var footerRow = document.createElement('tfoot');
+  var emptyCell = document.createElement('th');
+  footerRow.appendChild(emptyCell);
+  var hourlyStoreTotal = 0;
+  for (var i=0; i<hours.length; i++) {
+    hourlyStoreTotal = 0;
+    for(var j=0; j<locations.length; j++) {
+      hourlyStoreTotal += locations[j].hourlySales[i];
+      grandTotalSales += locations[j].hourlySales[i];
+    }
+    var hourlyTotalCell = document.createElement('th');
+    hourlyTotalCell.textContent = (hourlyStoreTotal);
+    footerRow.appendChild(hourlyTotalCell);
+  }
+  var grandTotalCell = document.createElement('th');
+  grandTotalCell.textContent = grandTotalSales;
+  footerRow.appendChild(grandTotalCell);
+  salesTable.appendChild(footerRow);
+}
+
+function renderTable() {
+  renderHeader();
+  renderBody();
+  renderFooter();
+}
+
+renderTable();
 
 // ## QUESTIONS ##
 // make getHourSales() global
